34b6d203af0e990190267f15ea10a86f815cbe6c
dilbatch <dilan_batchvarov@yahoo.com>
2014-05-22 21:14:15 +0300

    create models

---
+++ gumtree_assigment/src/com/gumtree/assigment/AddressBook.java
@@ -0,0 +1,18 @@
+package com.gumtree.assigment;
+
+import java.util.List;
+
+/**
+ * Created by Dilyan B. on 14-5-22.
+ */
+public class AddressBook {
+    private List contacts;
+
+    public List getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List contacts) {
+        this.contacts = contacts;
+    }
+}
---
+++ gumtree_assigment/src/com/gumtree/assigment/model/Contatct.java
@@ -0,0 +1,36 @@
+package com.gumtree.assigment.model;
+
+import java.util.Date;
+
+/**
+ * Created by Dilyan B on 14-5-22.
+ */
+public class Contatct {
+    private String name;
+    private String gender;
+    private Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
